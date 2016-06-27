172dbfd5f7070e0e8adc6c06c2b9c1050ba6a58e
mluzarow <mluzarow@purdue.edu>
2016-06-27 14:09:21 -0500

    Added files for the first part of LPTHW.

---
+++ Learn Python The Hard Way/ex01.py
@@ -0,0 +1,37 @@
+###############################################################################################
+## Learn Python The Hard Way                                                                 ##
+##      Exercise #1 : A Good First Program                                                   ##
+##      http://learnpythonthehardway.org/book/ex1.html                                       ##
+##                                                                                           ##
+## In this file you will complete the program requested of you in the exercise. As this is   ##
+## solely for the purposes of memorization of integral syntax, please don't just copy and    ##
+## paste the content into this file; rather, type it out into this editor.  Your program     ##
+## should output identically to the sample output listed below.                             ##
+##                                                                                           ##
+## ====Sample Output====                                                                     ##
+## Hello World!                                                                              ##
+## Hello Again                                                                               ##
+## I like typing this.                                                                       ##
+## This is fun.                                                                              ##
+## Yay! Printing.                                                                            ##
+## I'd much rather you 'not'.                                                                ##
+## I 'said' do not touch this.                                                               ##
+##                                                                                           ##
+## ====Note====                                                                              ##
+## This guide tells you to use Notepad, but we will be using Visual Studio.  While it does   ##
+## build proper programming habits and a deeper knowledge of Python, it is my opinion        ##
+## that those benefits come with time.  A True understanding of programming is that which    ##
+## you must have a want to pursue; that truly is all that is required to become a proper     ##
+## programmer.  As an addendum, VS will allow us to view runtime memory, pause programs,     ##
+## and explore Python errors in more detail, which I feel is a benefit for the student.      ##
+##                                                                                           ##
+## This book uses Python 2.x, or version 2 of Python.  There will be some errors thrown by   ##
+## Visual Studio in the process of using the book's code.  In the future, this note will be  ##
+## here to explain inconsistencies between Python 2.x and Python 3.x, and a method for       ##
+## translating the book's code into Python 3.x.                                              ##
+##                                                                                           ##
+## In this exercise, we use the "print" keyword.  In Python 3.x, the print statement must    ##
+## have its content enclosed by parenthesis. EG print "hey" should be print ("Hey")          ##
+###############################################################################################
+
+# Enter you code here (and delete this comment when you do)
---
+++ Learn Python The Hard Way/hw01.py
@@ -0,0 +1,29 @@
+###############################################################################################
+## Learn Python The Hard Way                                                                 ##
+##      Homework #1 : A Good First Program                                                   ##
+##      http://learnpythonthehardway.org/book/ex1.html                                       ##
+##                                                                                           ##
+## In this file you will build a program to fulfil the requested task. You should only use   ##
+## the input detailed in the summary, and your output must follow that of the sample output. ##
+## Always make sure to comment your code with meaningful comments; this may seem tedious,    ##
+## but will be worth it in the end.                                                          ##
+##                                                                                           ##
+## ====Task====                                                                              ##
+## You must make a program that will print out normal text to the console.  Your output      ##
+## should look identical to that of the sample output.                                       ##
+##                                                                                           ##
+## ====Sample Output====                                                                     ##
+## Hello, my name is Ben.                                                                    ##
+## I like Python a lot!                                                                      ##
+## Like, "a lot".                                                                            ##
+## I've made at least 1000 programs!                                                         ##
+##      *1000*                                                                               ##
+##                                                                                           ##
+## OK, maybe not.                                                                            ##
+##                                                                                           ##
+## ====Notes====                                                                             ##
+## print () in the Python 3.x documentation                                                  ##
+##      https://docs.python.org/3/library/functions.html#print                               ##
+###############################################################################################
+
+# Enter you code here (and delete this comment when you do)
